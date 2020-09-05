0cf721ce36f1847243aa6fe6c9381c9cf7bbcd5a
pomidorkaza <karpov-vb-1996@mail.ru>
2020-09-05 15:35:20 +0300

    cube-and-sphere

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, {useState, useRef } from 'react';
+import React, {useState, useRef, Suspense } from 'react';
 import { useSpring, a } from 'react-spring/three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-
-import {Canvas, useThree,extend, useFrame} from 'react-three-fiber';
+import {OrbitControls, Torus} from 'drei';
+import {TextureLoader} from 'three';
+import {Controls, useControl,ControlsProvider} from 'react-three-gui';
+import {Canvas, useThree,extend, useFrame, useLoader} from 'react-three-fiber';
+import imageUrl from './logo.svg';
 import './App.css';
 
 
-extend({OrbitControls});
+ 
 
-
-function Cube(props){
+function Sphere(props){
   const [isBig, setBig] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const ref = useRef();
-  const {size} = useSpring({
-    size: isBig ? [2,2,2]:[1,1,1] 
-  })
+  const {size,x,y,z} = useSpring({
+    size: isBig ? [2,2,2]:[1,1,1], 
+    x: isHovered && Math.random()> 0.5 ?  2:0,
+    y: isHovered && Math.random()> 0.5 ? 2:0,
+    z: isHovered && Math.random()> 0.5 ? 2:0,
+  
+  });
   const color = isHovered ?'pink':'salmon';
     useFrame(()=>{
     ref.current.rotation.x+=0.01;
@@ -23,37 +28,86 @@ function Cube(props){
   return (
     <a.mesh {...props}
     ref={ref}
+    position-x={x}
+    position-y={y}
+    position-z={z}
     scale ={ size}
     onPointerOver={()=> setIsHovered(true)}
     onPointerOut={()=> setIsHovered(false)}
     
     onClick={()=>setBig(!isBig)}
     >
-      <boxBufferGeometry attach="geometry" args={[1,1,1]}/>
+      <sphereBufferGeometry attach="geometry" args={[1,32,32]}/>
       <meshStandardMaterial attach="material" color={color}/>
     </a.mesh>
   )
 }
 
+function Cube(props){
+  const [isBig, setBig] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef();
+  const positionX = useControl('position X',{type: 'number'});
+
+  const texture =useLoader(TextureLoader, imageUrl);
+  const {size,x,y,z} = useSpring({
+    size: isBig ? [2,2,2]:[1,1,1], 
+    x: isHovered && Math.random()> 0.5 ?  2:0,
+    y: isHovered && Math.random()> 0.5 ? 2:0,
+    z: isHovered && Math.random()> 0.5 ? 2:0,
+  
+  });
+  const color = isHovered ?'pink':'salmon';
+    useFrame(()=>{
+    ref.current.rotation.x+=0.01;
+  });
+  return (
+    <a.mesh {...props}
+    ref={ref}
+    position-x={positionX}
+    position-y={y}
+    position-z={z}
+    scale ={ size}
+    onPointerOver={()=> setIsHovered(true)}
+    onPointerOut={()=> setIsHovered(false)}
+    
+    onClick={()=>setBig(!isBig)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1,1,1,20,20,20]}/>
+      <meshStandardMaterial 
+      map={texture}
+      attach="material" color={color}/>
+    </a.mesh>
+  )
+}
+
 function Scene(){
-  const {
-    camera,
-    gl:{
-      domElement
-    }
-  } = useThree();
-  return (<>
+   return (<>
   <ambientLight/>
-  <pointLight position={[-1,2,4]}/>
-  <Cube  rotation={[1,2,10]} position={[0,0,0]}/>
-  <orbitControls args={[camera, domElement]}/>
-  </>);
+  <spotLight castShadow={true} intensity={0.6} position={[0,4,10]}/>
+  <pointLight position={[1,2,4]}/>
+  <Suspense fallback={null}>
+  <Cube/>
+  </Suspense>
+  <Torus args={[ 1,0.2,3,32]} position={[1,2,2]}>
+    <meshPhongMaterial 
+    roughness={1} 
+    metalness={0.5} 
+    shininess={100}
+    color="blue"
+    attach="material"
+    />
+  </Torus>
+    </>);
 }
 function App() {
   return (
+    <ControlsProvider>
     <Canvas >
       <Scene/> 
     </Canvas>
+  <Controls/>
+  </ControlsProvider>
   );
 }
 
